54a6eed536f2899da9f91ef937d00bac7c349195
ollo <ollo@mobileLnx.(none)>
2012-12-29 15:02:28 +0100

    Created a new library function for drawing lines

---
+++ client-redLine.py
@@ -0,0 +1,64 @@
+__author__ = 'bene'
+
+#!/opt/local/bin/python2.7
+#
+# Some protobuf content for python:
+# http://eigenein.info/protobuf/
+
+from optparse import OptionParser
+import sys
+import socket
+from PyFullcircle import FcClient, FcFrame, FcPixel
+
+
+class RedDotAni():
+
+    dotPosX = 0
+    dotPosY = 0
+    dotColorR = 0x00
+    dotColorG = 0x12
+    dotColorB = 0x87
+
+    def frameupdate(self,w,h):
+
+        frame = FcFrame(w,h)
+
+       # print "X: %i Y: %i" % (self.dotPosX, self.dotPosY)
+       # print frame
+
+        frame.setColorForPixel(self.dotPosX, self.dotPosY, self.dotColorR, self.dotColorG, self.dotColorB)
+
+        self.dotPosX += 1
+        self.dotPosY += 1
+
+        if self.dotPosX >= w or self.dotPosY >= h:
+           return None
+
+        return frame.getProtobufPkt()
+
+def main():
+    usage = "usage: %prog [options] arg1 arg2"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-t", "--target",
+        action="store", type="string", dest="target")
+    (options, args) = parser.parse_args()
+    print "Target"  , options.target
+
+    fps = 10
+    width = 10
+    height = 12
+
+    rda = RedDotAni()
+
+    try:
+        client = FcClient(options.target)
+        client.send_frames(fps, width, height, rda.frameupdate)
+    except socket.error as msg:
+        sys.stderr.write("[ERROR] %s\n" % msg[1])
+        parser.print_help()
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    main()
